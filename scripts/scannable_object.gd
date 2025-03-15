94e004bb9646997c6a9f90633df676315d88c4b3
Tom Woodling <t.woodling@coolblue.nl>
2025-03-15 14:03:39 +0100

    default scannable object added

--- scripts/scannable_object.gd
+++ scripts/scannable_object.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends StaticBody3D
 class_name ScannableObject
 
 @export_enum("Exobiology", "Exobotany", "Exogeology", "Artifacts") var scan_category: int = 0
@@ -6,10 +6,24 @@ class_name ScannableObject
 @export var object_name: String = "Unknown Object"
 @export_multiline var description: String = "No data available"
 
+@onready var scan_highlight: OmniLight3D = $ScanHighlight
+@onready var scan_indicator: GPUParticles3D = $ScanIndicator
+
 var has_been_scanned: bool = false
+var in_range: bool = false
+
+func _ready() -> void:
+	# Set up default material if none provided
+	if $MeshInstance3D.get_surface_override_material(0) == null:
+		var default_mat = StandardMaterial3D.new()
+		default_mat.albedo_color = Color(0.7, 0.7, 0.8)
+		default_mat.metallic = 0.3
+		default_mat.roughness = 0.7
+		$MeshInstance3D.set_surface_override_material(0, default_mat)
 
 func get_scan_data() -> Dictionary:
 	has_been_scanned = true
+	scan_highlight.visible = false
 	return {
 		"name": object_name,
 		"category": ScannerManager.ScanCategory.values()[scan_category],
@@ -20,3 +34,8 @@ func get_scan_data() -> Dictionary:
 
 func get_scan_difficulty() -> float:
 	return scan_difficulty
+
+func highlight_in_range(is_in_range: bool) -> void:
+	in_range = is_in_range
+	scan_highlight.visible = is_in_range and not has_been_scanned
+	scan_indicator.visible = is_in_range and not has_been_scanned
